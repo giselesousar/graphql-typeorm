a23939a89d56b9e17e24a4fb61bad556fa85a0b9
Gisele <giselesousar@gmail.com>
2020-10-20 11:47:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+import { connection } from './config/typeorm';
+import { startServer } from './server';
+
+async function main() {
+    connection();
+    const app = await startServer();
+    app.listen(4000);
+    console.log("Server on port", 4000)
+}
+
+main();
---
+++ src/resolvers/ProductResolver.ts
@@ -0,0 +1,56 @@
+import { Resolver, Query, Mutation, Arg, Field, InputType, Int } from 'type-graphql';
+
+import { Product } from '../entity/Product';
+
+@InputType()
+class ProductInput {
+
+    @Field()
+    name!: string
+
+    @Field()
+    quantity!: number
+}
+
+@InputType()
+class ProductUpdateInput {
+    @Field(() => String, {nullable: true})
+    name?: string;
+
+    @Field(() => Int, {nullable: true})
+    quantity?: number;
+}
+
+@Resolver()
+export class ProductResolver {
+
+    @Mutation(() => Product)
+    async createProduct(
+        @Arg("variables", () => ProductInput) variables: ProductInput
+    ) {
+        const newProduct =  Product.create(variables);
+        return await newProduct.save();
+    }
+
+    @Mutation(() => Boolean)
+    async deleteProduct(
+        @Arg("id", () => Int) id: number
+    ){
+        await Product.delete(id);
+        return true;
+    }
+
+    @Mutation(() => Boolean)
+    async updateProduct(
+        @Arg("id", () => Int)id: number, 
+        @Arg("newData", () => ProductUpdateInput) newData: ProductUpdateInput
+    ){
+        await Product.update({id}, newData);
+        return true;
+    }
+
+    @Query(() => [Product])
+    products() {
+        return Product.find()
+    }
+}
